f5d21aba5ea6b544abd95bde1886c718f86c1e2e
Akshyadav07 <golu2116mhp@gmail.com>
2025-03-10 11:25:42 +0530

    datatypes and variable

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; /* treat all js code as newer version */
+
+// alert(3+3); this method is not for nodejs.
+
+console.log(3+3)
+console.log("mynk")
+
+// datatypes 
+let name = "mynk" /* string */
+let age = 18 /* age */ 
+let islog = false /* boolean */
+let whoseAccount;
+
+// number => 2 to power 53
+// bigint for more greater value 
+// string => ""
+// boolean is false or true 
+// null standalone value
+// undefined is for undefined value
+// symbol for unique value
+
+// we can find the string type value from this 
+console.log(typeof "18")
+
+console.log(typeof null)
+console.log(typeof undefined);
---
+++ variable.js
@@ -0,0 +1,13 @@
+const accountId = 122443;
+let accountemail = "mynk@gmail";
+var accountpass = "mynk.com";
+accountcity = "jaipur";
+let whoseAccount;
+
+
+accountemail = "aksh@gmail.com";
+accountpass = "aksh.com";
+accountcity = "kanpur";
+
+console.log(accountId);
+console.table([accountId,accountemail,accountpass,accountcity,whoseAccount])
