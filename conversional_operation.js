b2b72a7ce1c2883245fc06a1a15fcffe4dadd0f7
Akshyadav07 <golu2116mhp@gmail.com>
2025-03-12 22:47:32 +0530

    conversion of datatypes

---
+++ conversional_operation.js
@@ -0,0 +1,59 @@
+// if value is number
+
+/* let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// if value is combine
+
+/* let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// if value is null
+
+/* let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// if value is undifened
+
+/* let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// "33" => 33
+// "33abc" => NaN
+//  True => 1; false => 0;
+
+// how to chack 1 and 0 datatype
+
+/* let isLoggedIn = 1
+let booleanIsLoggedIn = boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); */
+
+// we can also change a number into a String
+let number = 33
+let stringNumber = String(number)
+console.log(typeof (stringNumber));
+console.log(stringNumber);
