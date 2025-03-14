a7dbc3b88c60379eb0e08abafac779a6c1a42c02
Akshyadav07 <golu2116mhp@gmail.com>
2025-03-14 23:16:05 +0530

    operations

---
+++ operations.js
@@ -0,0 +1,27 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 ="hello"
+let str2 = " mynk"
+console.log(str1 + str2);
+
+console.log("1"+ 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3")
+
+console.log( (3+4) * 5 % 3);
+
+console.log(true); /* answer of this value is 1 */
+console.log(+""); /* answer of this value is 0 */
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+
+
