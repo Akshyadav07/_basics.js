f7c9091c0313b6a2996743fd5046e33e1c22de30
Akshyadav07 <golu2116mhp@gmail.com>
2025-03-23 11:16:22 +0530

    maths_and_numbers

---
+++ 03datatypes.js
@@ -0,0 +1,33 @@
+const name = "aksh ";
+const repocount = 50;
+
+// console.log(name + repocount + " value");
+
+console.log(`hello, my name is ${name} and my repocount is ${repocount}`);
+
+const gameName = new String("akash-yadav")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.lenght);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4)); //we can also use indexOf in the location of charAt
+
+const newString = gameName.substring(0, 6)
+console.log(newString);
+
+ const anotherString = gameName.slice(-3, 4)
+ console.log(anotherString);
+
+ const oneString = "     aksh"
+ console.log(oneString);
+ console.log(oneString.trim());
+ 
+ const url = "https://aksh@gmail.com"
+
+ console.log(url.replace('%20', '-'));
+ 
+console.log(url.includes('sunder'));
+
+
+ 
